a1b89eb7012358caa4c5a397f668c9cba510b413
nokori <69152061+ucelery@users.noreply.github.com>
2023-10-27 13:44:58 +0800

    Implement Add and Edit modals

--- src/app/show-dogs/show-dogs.component.ts
+++ src/app/show-dogs/show-dogs.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Dog } from '../model/dog';
 
 @Component({
   selector: 'app-show-dogs',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-dogs.component.css']
 })
 export class ShowDogsComponent {
+  @ViewChild('DeletePopup') DeletePopup: ElementRef;
+  @ViewChild('EditPopup') EditPopup: ElementRef;
+  @ViewChild('AddPopup') AddPopup: ElementRef;
+
+  delete_selected_id = ""
+  visible_pop_up = false
   
+  constructor() {
+    this.DeletePopup = new ElementRef(null);
+    this.EditPopup = new ElementRef(null);
+    this.AddPopup = new ElementRef(null);
+  }
+
+  // POP-UP FOR DELETE
+  toggleDeleteModal(id: string) {
+    this.delete_selected_id = id === "" ? "ERR: no_id_passed" : id;
+
+    if (this.visible_pop_up)  {
+      // HIDE
+      this.DeletePopup.nativeElement.classList.add('hide');
+      this.visible_pop_up = false
+    } else {
+      // SHOW
+      this.DeletePopup.nativeElement.classList.remove('hide')
+      this.visible_pop_up = true
+    }
+  }
+
+  // POP-UP FOR ADD
+  toggleAddModal() {
+    if (this.visible_pop_up)  {
+      // HIDE
+      this.AddPopup.nativeElement.classList.add('hide');
+      this.visible_pop_up = false
+    } else {
+      // SHOW
+      this.AddPopup.nativeElement.classList.remove('hide')
+      this.visible_pop_up = true
+    }
+  }
+
+  // POP-UP FOR EDIT
+  toggleEditModal(id: string) {
+    if (this.visible_pop_up)  {
+      // HIDE
+      this.EditPopup.nativeElement.classList.add('hide');
+      this.visible_pop_up = false
+    } else {
+      // SHOW
+      this.EditPopup.nativeElement.classList.remove('hide')
+      this.visible_pop_up = true
+    }
+
+    console.log(this.EditPopup)
+  }
+
+  deleteDog() {
+    console.log(`Dog "${this.delete_selected_id}" has been deleted`);
+    this.toggleDeleteModal('');
+  }
+
+  addDog() {
+    console.log("add dog");
+  }
+
+  editDog(id: string) {
+    console.log(`Edited ${id}`);
+  }
 }
